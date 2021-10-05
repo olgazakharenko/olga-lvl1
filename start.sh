503a5508045e829ba818cefc6eb0a7732a1c8ad9
Olga Zakharenko <olga.zakharenko@takeoff.com>
2021-10-05 17:21:21 -0400

    update docker image before deploy

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ project_id="roi-takeoff-user94"
 if [ $GOOGLE_CLOUD_PROJECT == "" ]; then
 	export GOOGLE_CLOUD_PROJECT=$project_id
 fi
+gcloud auth login
+gcloud builds submit --tag=gcr.io/$GOOGLE_CLOUD_PROJECT/go-pets:latest .
 cd infra
 terraform init && terraform apply -auto-approve
 
